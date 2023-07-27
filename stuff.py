b99108960293c070842d64be99dff5c9c97f0efb
MrCool08 <liamsuorsa08@gmail.com>
2023-07-27 15:33:37 +0200

    added a confirm button on the confirmation page

--- stuff.py
+++ stuff.py
@@ -10,8 +10,7 @@ def booking_exists(name, email, phone, language, time_start, time_end):
     cursor = conn.cursor()
 
     # Check if the booking already exists in the database
-    cursor.execute("SELECT COUNT(*) FROM bookings WHERE name = ? AND email = ? AND phone = ? AND language = ? AND time_start = ? AND time_end = ?",
-                   (name, email, phone, language, time_start, time_end))
+    cursor.execute("SELECT COUNT(*) FROM bookings WHERE name = ? AND email = ? AND phone = ? AND language = ? AND time_start = ? AND time_end = ?", (name, email, phone, language, time_start, time_end))
     count = cursor.fetchone()[0]
 
     # Close the database connection
