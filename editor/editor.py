820de535e0ab844c83a9aebdd029509578eae03f
Liam server <liam@suorsa.se>
2024-02-01 20:06:12 +0000

    security

--- editor/editor.py
+++ editor/editor.py
@@ -94,4 +94,4 @@ def move_to_taken_bookings(booking_id):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
